097acb8b3cf782f8de0c60ce66a9d95b31ee6f2a
Kevin Sherman <kevsherman@gmail.com>
2014-01-24 20:52:40 -0800

    added quoting functions

--- public/index.php
+++ public/index.php
@@ -6,11 +6,9 @@
     //get users info from db
 
     $user = query("SELECT * FROM users WHERE id = ?", $_SESSION["id"]);
-
-//    dump($user);
     
 
     // render portfolio and pass in $user Array which contains all the users data from db
-    render("portfolio.php", [ "title" => "Portfolio", "user" => $user  ]);
+    render("portfolio.php", [ "title" => "Portfolio", "user" => $user]);
 
 ?>
--- templates/portfolio.php
+++ templates/portfolio.php
@@ -1,6 +1,34 @@
+<?php
+/*
+//  This page shows the current users stock portolio 
+//
+//  Variables are passed in from index.php via $users, which called all db rows for the user:
+//  Eg:
+//            Array
+//                    (
+//                   [0] => Array
+//                        (
+//                           [id] => 20
+//                           [username] => newguy12
+//                           [hash] => $1$V411.CmR$JTOkUNRexX40YIdbBRwtP1
+//                           [cash] => 10000.0000
+//                        )
+//
+//                    )
+//
+/////////////////////////////////////////////////////////////////////////////////////////*/ ?>
+
 <div>
     <h2>Welcome <?= $user[0]["username"] ?> </h2>
 </div>
+
+<ul>
+    <li>Available Cash: <?= $user[0]["cash"] ?></li>
+    <li><h2>Current Stocks</h2></li>
+</ul>
+
+<div>
+    <a href="quote.php">Seach Stocks by Symbol </a>
 <div>
     <a href="logout.php">Log Out</a>
 </div>
